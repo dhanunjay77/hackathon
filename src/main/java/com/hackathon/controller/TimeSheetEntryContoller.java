9612bbe4e068f9d844ef2d04293271ed78832b5c
madhan <mgl12432@gmail.com>
2025-06-14 16:27:58 +0530

    Added chagnes

--- src/main/java/com/hackathon/controller/TimeSheetEntryContoller.java
+++ src/main/java/com/hackathon/controller/TimeSheetEntryContoller.java
@@ -4,8 +4,10 @@ import com.hackathon.dto.TimeSheetEntryDto;
 import com.hackathon.entities.TimeSheetEntry;
 import com.hackathon.service.TimeSheetEntryService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,8 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/timesheetentry")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('TIMESHEETEntry')")
 public class TimeSheetEntryContoller {
 
     @Autowired
